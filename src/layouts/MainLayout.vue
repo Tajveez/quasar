2f57fd0078564fdcacec5b347ff5e2961a4b0d64
Tajveez <Tajveez786@gmail.com>
2021-03-15 23:04:24 +0500

    Drawer done

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -18,19 +18,19 @@
       </q-tabs>
     </q-header>
 
-    <q-drawer show-if-above v-model="left" side="left" bordered>
+    <q-drawer :width="280" show-if-above v-model="left" side="left" bordered>
       <!-- drawer content -->
       <q-icon class="q-pa-md" name="fas fa-crow" size="lg" color="primary" />
       <q-list>
         <q-item to="/" clickable v-ripple>
           <q-item-section avatar>
-            <q-icon color="primary" name="home" size="md"/>
+            <q-icon name="home" size="md"/>
           </q-item-section>
           <q-item-section class="text-h6 text-weight-bold">Home</q-item-section>
         </q-item>
         <q-item to="/about" clickable v-ripple>
           <q-item-section avatar>
-            <q-icon color="primary" name="help" size="md"/>
+            <q-icon name="help" size="md"/>
           </q-item-section>
           <q-item-section class="text-h6 text-weight-bold">About</q-item-section>
         </q-item>
