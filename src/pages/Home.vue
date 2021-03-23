0b2eadbf0a8e7d8d5f8f6c4c2b5fcfdf56d09ddd
tajveez <tajveez786@gmail.com>
2021-03-23 17:33:47 +0500

    Homepage building

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,42 @@
 <template>
   <q-page>
-    <h5>Hello world !</h5>
+    <div class="q-py-lg q-px-md">
+      <q-input
+        v-model="newTwittoContent"
+        bottom-slots
+        placeholder="Write your thoughts"
+        counter
+        maxlength="300"
+      >
+        <template v-slot:before>
+          <q-avatar size="xl">
+            <img src="https://cdn.quasar.dev/img/avatar5.jpg" />
+          </q-avatar>
+        </template>
+
+        <template v-slot:after>
+          <q-btn
+            unelevated
+            rounded
+            color="primary"
+            label="Twitt"
+            no-caps
+            :disable="!newTwittoContent"
+          />
+          <!-- <q-btn round dense flat icon="send" /> -->
+        </template>
+      </q-input>
+    </div>
   </q-page>
 </template>
 
 <script>
 export default {
   name: "PageHome",
+  data() {
+    return {
+      newTwittoContent
+    };
+  }
 };
 </script>
