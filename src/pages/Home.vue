5022bff36747e2b3301d2c5e5c0f2beb9f7bc31d
tajveez <tajveez786@gmail.com>
2021-03-23 17:49:19 +0500

    Home page add tweet done

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,32 +1,36 @@
 <template>
   <q-page>
-    <div class="q-py-lg q-px-md">
-      <q-input
-        v-model="newTwittoContent"
-        bottom-slots
-        placeholder="Write your thoughts"
-        counter
-        maxlength="300"
-      >
-        <template v-slot:before>
-          <q-avatar size="xl">
-            <img src="https://cdn.quasar.dev/img/avatar5.jpg" />
-          </q-avatar>
-        </template>
-
-        <template v-slot:after>
-          <q-btn
-            unelevated
-            rounded
-            color="primary"
-            label="Twitt"
-            no-caps
-            :disable="!newTwittoContent"
-          />
-          <!-- <q-btn round dense flat icon="send" /> -->
-        </template>
-      </q-input>
+    <div class="q-py-lg q-px-md row items-end q-col-gutter-md">
+      <div class="col">
+        <q-input
+          v-model="newTwittoContent"
+          class="new-twitt"
+          bottom-slots
+          placeholder="Write your thoughts"
+          counter
+          autogrow
+          maxlength="300"
+        >
+          <template v-slot:before>
+            <q-avatar size="xl">
+              <img src="https://cdn.quasar.dev/img/avatar5.jpg" />
+            </q-avatar>
+          </template>
+        </q-input>
+      </div>
+      <div class="col col-shrink">
+        <q-btn
+          class="q-mb-lg"
+          unelevated
+          rounded
+          color="primary"
+          label="Twitt"
+          no-caps
+          :disable="!newTwittoContent"
+        />
+      </div>
     </div>
+    <q-separator class="divider" size="10px" color="grey-2" />
   </q-page>
 </template>
 
@@ -40,3 +44,13 @@ export default {
   }
 };
 </script>
+<style lang="sass">
+.new-twitt
+  textarea
+    font-size: 19px
+    line-height: 1.4
+.divider
+  border-top: 1px solid
+  border-bottom: 1px solid
+  border-color: $grey-4
+</style>
